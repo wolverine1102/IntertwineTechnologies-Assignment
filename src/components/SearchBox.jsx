b83728fd34cf005b152bf7a2f31e129fb375e9fc
Shreyosi Sarkar <21f1003968@ds.study.iitm.ac.in>
2023-12-27 18:14:02 +0530

    Update package.json

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,13 +1,13 @@
 import { InputText } from 'primereact/inputtext';
 
-export default function SearchBox({ value, searchFunction, size, style }) {
+export default function SearchBox({ value, handleChange, style }) {
     return (
         <span className="p-input-icon-left">
             <i className="pi pi-search" />
             <InputText placeholder="Search"
+                       type='text'
                        value={value} 
-                       onInput={searchFunction} 
-                       className={size} 
+                       onChange={handleChange}
                        style={style} />
         </span>
     )
--- src/views/UserTable.jsx
+++ src/views/UserTable.jsx
@@ -4,7 +4,7 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import SearchBox from "../components/SearchBox";
-import _ from lodash;
+import _ from "lodash";
 
 
 const baseURL = "api/users";
@@ -43,51 +43,52 @@ export default function UserTable() {
       </div>
     )
   }
-  
-  const deleteRow = (rowData) => {
-    const name = `${rowData.name.first} ${rowData.name.last}`;
-    users.filter
-  
+
+  const handleDelete = (username) => {
+    const updatedUsers = users.filter((user) => !(username.match(user.login.username)));
+    setUsers(updatedUsers);
   }
 
-  const deleteButton = () => {
+  const deleteButton = (username) => {
     return (
-      <Button label="Delete" severity="warning" 
-              icon="pi pi-trash" rounded 
-              style={{ fontWeight: 'bold', color: 'white' }}
-              onClick={deleteRow}
-              />
+      <Button label="Delete" severity="warning"
+        icon="pi pi-trash" rounded
+        style={{ fontWeight: 'bold', color: 'white' }}
+        onClick={() => handleDelete(username)}
+      />
     )
   }
-  
-  // const globalSearch = users.filter((user) => {
-  //   if (searchInput === "") {
-  //     return user;
-  //   }
-  //   else {
-  //     return user.name.toLowerCase().match(searchInput);
-  //   }
-  // })
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const filteredUsers = users.filter((user) => {
+    return (
+      user.gender.toLowerCase() === (searchInput.toLowerCase()) ||
+      user.name.first.toLowerCase().includes(searchInput.toLowerCase()) ||
+      user.name.last.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  });
 
   return (
     <div>
       <div className="container" style={{ margin: '15px', display: 'flex', justifyContent: 'space-between' }}>
         <h1 style={{ color: '#37404c', letterSpacing: '2px' }}>Users</h1>
-        <SearchBox value={searchInput} style={{ width: '700px', borderRadius: '15px', marginTop: '18px' }} />
+        <SearchBox value={searchInput} style={{ width: '700px', borderRadius: '15px', marginTop: '18px' }} 
+          handleChange={handleChange}
+        />
         <div></div>
       </div>
-      <DataTable value={users} 
-                 sortMode="multiple" paginator rows={7} 
-                 stripedRows tableStyle={{ minWidth: '50rem' }}
-                 style={{ margin: '15px', marginTop: '25px' }}>
-        <Column header="Name" sortable body={combinedName} 
-                bodyStyle={{ fontWeight: 'bold' }}
-                sortField="name.first"></Column>
-        <Column field="gender" sortable header="Gender" bodyStyle={{ textTransform: 'capitalize' }}></Column>
-        <Column body={combinedLocation} header="Address"></Column>
-        <Column field="email" header="Email"></Column>
-        <Column field="phone" header="Phone"></Column>
-        <Column body={deleteButton}></Column>
+      <DataTable value={filteredUsers} sortMode="multiple" paginator rows={7} stripedRows tableStyle={{ minWidth: '50rem' }}
+        style={{ margin: '15px', marginTop: '25px' }} globalFilter={searchInput}>
+        <Column header="Name" sortable body={combinedName} bodyStyle={{ fontWeight: 'bold' }}
+          sortField="name.first" />
+        <Column field="gender" sortable header="Gender" bodyStyle={{ textTransform: 'capitalize' }} />
+        <Column body={combinedLocation} header="Address" />
+        <Column field="email" header="Email" />
+        <Column field="phone" header="Phone" />
+        <Column body={(rowData) => (deleteButton(rowData.login.username))} />
       </DataTable>
     </div>
   )
